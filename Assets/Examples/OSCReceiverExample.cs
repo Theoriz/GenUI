ccc210dcb109fe78bbda147a870c91e5fac3c202
AlexBourgeois <abourgeois27380@gmail.com>
2017-12-21 12:41:09 +0100

    Cleaned osc example

--- Assets/Examples/OSCReceiverExample.cs
+++ Assets/Examples/OSCReceiverExample.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OSCReceiver : MonoBehaviour
+public class OSCReceiverExample : MonoBehaviour
 {
 
     private OSCMaster oscMaster;
