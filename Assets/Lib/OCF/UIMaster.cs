9b111527fd615f9da29b9141d3625da259b4f92a
AlexBourgeois <abourgeois27380@gmail.com>
2018-02-01 10:29:10 -0600

    Removed UI at start

--- Assets/Lib/OCF/UIMaster.cs
+++ Assets/Lib/OCF/UIMaster.cs
@@ -32,6 +32,8 @@ public class UIMaster : MonoBehaviour
 
         ControllableMaster.controllableAdded += CreateUI;
         ControllableMaster.controllableRemoved += RemoveUI;
+
+        ToggleUI();
     }
 
     public void ToggleUI()
