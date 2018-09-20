6c4e9f6bebf4edf33d05609ffd151b05d1470878
AlexBourgeois <abourgeois27380@gmail.com>
2018-09-20 19:44:46 +0200

    Temp fix for Unity panel scale bug

---
+++ Lib/Scripts/ShouldNotExist.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShouldNotExist : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = Vector3.one;		
+	}
+}
