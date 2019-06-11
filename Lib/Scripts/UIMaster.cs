79d3094f83add513888d3d57409d245a9370dda9
alexbourgeois <abourgeois27380@gmail.com>
2019-06-11 15:24:58 +0200

    Changed public bool to private as they should be

--- Lib/Scripts/UIMaster.cs
+++ Lib/Scripts/UIMaster.cs
@@ -37,9 +37,9 @@ public class UIMaster : MonoBehaviour
     private GameObject _rootCanvas;
     private Dictionary<string, GameObject> _panels;
 
-    public bool _rightClickMenuInstantiated;
-    public bool _skipNextButton;
-    public bool _destroyMenuOnNextFrame;
+    private bool _rightClickMenuInstantiated;
+    private bool _skipNextButton;
+    private bool _destroyMenuOnNextFrame;
     private GameObject _rightClickMenu;
 
     public static UIMaster Instance;
